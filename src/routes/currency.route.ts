14db33e9c680af7540b13070a4c0dd3257a07654
Adil Arif <arifadil739@gmail.com>
2024-07-29 20:57:36 +0500

    test

--- src/routes/currency.route.ts
+++ src/routes/currency.route.ts
@@ -1,5 +1,5 @@
 import express from 'express'; 
-import * as currencyService from "../services/currency.service"
+import * as currencyService from "../services/currency.service";
 const router = express.Router();
 
 router.get('/all',currencyService.getCurrencies);
