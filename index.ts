5adeffdb4f7e5413394f146d93513fc678864627
Adil Arif <arifadil739@gmail.com>
2024-07-29 20:44:56 +0500

    test

--- index.ts
+++ index.ts
@@ -1,17 +1,3 @@
-// const express = require("express");
-
-// const app = express();
-
-// app.get("/", (req, res) => {
-//   res.send("Express on Vercel changed");
-// });
-
-// app.listen(5000, () => {
-//   console.log("Running on port 5000.");
-// });
-
-// // Export the Express API
-// module.exports = app;
 import express, { NextFunction, Request, Response } from 'express';
 import createHttpError from 'http-errors';
 const app = express();
