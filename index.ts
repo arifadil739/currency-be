9b503c36159d18f02f9e69e4df572b89405dd406
Adil Arif <arifadil739@gmail.com>
2024-07-29 20:41:25 +0500

    check

--- index.ts
+++ index.ts
@@ -1,14 +1,43 @@
-const express = require("express");
+// const express = require("express");
 
+// const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("Express on Vercel changed");
+// });
+
+// app.listen(5000, () => {
+//   console.log("Running on port 5000.");
+// });
+
+// // Export the Express API
+// module.exports = app;
+import express, { NextFunction, Request, Response } from 'express';
+import createHttpError from 'http-errors';
 const app = express();
+const port = process.env.PORT || 3001;
+const ui_base_url = process.env.UI_BASE_URL
+const cors = require("cors");
 
-app.get("/", (req, res) => {
-  res.send("Express on Vercel changed");
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, world!');
 });
 
-app.listen(5000, () => {
-  console.log("Running on port 5000.");
+const currencyRouter = require("./routes/currency.route");
+app.use("/currency", currencyRouter);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(createHttpError(404, 'Not Found'));
 });
 
-// Export the Express API
-module.exports = app;
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({ error: err.message || 'Internal Server Error' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
